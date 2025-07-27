9d01c8a416b176209b56a60c3c3ab5402276fb2a
Priyanka <priyanka.rel6@gmail.com>
2025-07-27 19:39:10 +0530

    added k8s installation steps in docker.sh file

--- docker.sh
+++ docker.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 echo "=== Script started at: $(date) ===" | tee /var/log/init-script-timer.log
+
+## Docker Installation
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+
+## Disk resize
 growpart /dev/nvme0n1 4
 lvextend -L +15G /dev/RootVG/rootVol
 lvextend -L +10G /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
+
 ## Eksctl Installation
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
@@ -19,13 +24,17 @@ curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
 ## Kubectl Installation
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
+
 ## Kubens Installation
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
 ## K9s Installation
 #  curl -sS https://webinstall.dev/k9s | bash
+
 echo "=== Script ended at: $(date) ===" | tee -a /var/log/init-script-timer.log
