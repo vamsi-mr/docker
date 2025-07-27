e30447bc7c2b0dcf3b8a5e7717903407775dada8
Priyanka <priyanka.rel6@gmail.com>
2025-07-27 19:15:38 +0530

    added k8s installation steps in docker.sh file

--- docker.sh
+++ docker.sh
@@ -11,13 +11,14 @@ lvextend -L +15G /dev/RootVG/rootVol
 lvextend -L +10G /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
-## Kubernetes Installation
+## Eksctl Installation
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+## Kubectl Installation
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
@@ -25,4 +26,5 @@ sudo mv kubectl /usr/local/bin/kubectl
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 ## K9s Installation
-curl -sS https://webinstall.dev/k9s | bash
+#  curl -sS https://webinstall.dev/k9s | bash
+
