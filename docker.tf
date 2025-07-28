ab6f1cda2dae958757504f229bbd5c94ec35da26
Priyanka <priyanka.rel6@gmail.com>
2025-07-28 15:12:04 +0530

    added am instance profile to create eks cluster

--- docker.tf
+++ docker.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "docker" {
   ami = local.ami_id
   instance_type = "t3.medium"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  iam_instance_profile = "AWS-EKS"
   #need more volume for 
   root_block_device {
     volume_size = 50
