6ed7dfd8b476d50af9d046405d8e2187ca756562
Priyanka <priyanka.rel6@gmail.com>
2025-08-04 10:23:08 +0530

    changed instance state to t3 micro from t3 medium

--- docker.tf
+++ docker.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "docker" {
   ami = local.ami_id
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
   iam_instance_profile = "AWS-EKS"
   #need more volume for 
